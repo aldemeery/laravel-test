b9f6aef2ec27c50dd3b2691744cce6496299ae2a
Osama Aldemeery <aldemeery@gmail.com>
2020-10-11 03:05:25 +0200

    Disallow unauthenticated users from accessing the API.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PostController;
 
-// TODO: Only authenticated users have access.
-Route::apiResource('posts', PostController::class)->only('index', 'store', 'show');
-Route::apiResource('posts/{post}/comments', CommentController::class)->only('store');
-Route::get('notifications', NotificationController::class);
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('posts', PostController::class)->only('index', 'store', 'show');
+    Route::apiResource('posts/{post}/comments', CommentController::class)->only('store');
+    Route::get('notifications', NotificationController::class);
+});
